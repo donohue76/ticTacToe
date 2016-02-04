a0cc6264c72213cbafdf344c0406faf33c1029dc
LEARN <learn@LEARN03.local>
2016-02-04 13:25:18 -0800

    day 2 commit

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,117 +1,71 @@
+var boardArray = [0,0,0,0,0,0,0,0,0];
+var clicks = 0;
 $(document).ready(function(){
-  var boardArray = [0,0,0,0,0,0,0,0,0];
-  var winningCombos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6],[0,4,8]];
-  var clicks = 0;
   $("#playerInputs").hide();
+  $("#playerDisplay").hide();
   $("#start").click(function(){
-    $(this).hide();
+    $("#startBtn").hide();
     $("#playerInputs").show();
   })
-  $(".box").click(function(){
-    $(this).unbind('click');
-    if (clicks % 2 === 0){
-      $(this).css('background-image', 'url("images/x.png")');
-      ++clicks;
-      boardArray[parseInt(this.id)] = 1;
+  $("#submit").click(function(){
+    var player1 = $("#player1").val();
+    var player2 = $("#player2").val();
+    if (player1.length <= 0 || player2.length <=0) {
+      alert("Please enter a name");
+    } else {
+      $("#playerInputs").hide();
+      $("#names h2").html(player1 + " vs. " + player2);
+      $("#playerDisplay").show();
+      ticTacToeGame();
     }
-    else if (clicks % 2 === 1) {
-      $(this).css('background-image', 'url("images/o.png")');
-      ++clicks;
-      boardArray[parseInt(this.id)] = -1;
+  })
+  // $("#reset").click(function(){
+  //   $(".box").bind('click');
+  //   $(".box").css('background-image','');
+  // })
+  function whoWon(arr) {
+    if(findSum(arr) === 3) {
+      return "X is the winner";
+    } else if (findSum(arr) === -3) {
+      return "O is the winner";
+    } else if (findSum(arr) === 0) {
+      return "No winner yet";
     };
+  };
+  function findSum(arr) {
+    if (sum(arr,0,1,2) === 3 || sum(arr,3,4,5) === 3 || sum(arr,6,7,8) === 3 || sum(arr,0,3,6) === 3 || sum(arr,1,4,7) === 3 || sum(arr,2,5,8) === 3 || sum(arr,2,4,6) === 3 || sum(arr,0,4,8) === 3) {
+      return 3;
+    } else if (sum(arr,0,1,2) === -3 || sum(arr,3,4,5) === -3 || sum(arr,6,7,8) === -3 || sum(arr,0,3,5) === -3 || sum(arr,1,4,7) === -3 || sum(arr,2,5,8) === -3 || sum(arr,2,4,6) === -3 || sum(arr,0,4,8) === -3) {
+      return -3;
+    } else {
+      return 0;
+    };
+  };
+  function sum(arr,x,y,z) {
+    return arr[x] + arr[y] + arr[z];
+  };
+  function ticTacToeGame(){
+    $(".box").click(function(){
+      $(this).off('click');
+      if (clicks % 2 === 0){
+        $(this).css('background-image', 'url("images/x.png")');
+        ++clicks;
+        boardArray[parseInt(this.id)] = 1;
+      } else if (clicks % 2 === 1) {
+        $(this).css('background-image', 'url("images/o.png")');
+        ++clicks;
+        boardArray[parseInt(this.id)] = -1;
+      };
+      if (clicks === 9) {
+        alert("Game Over")
+      };
+      console.log(boardArray);
+      console.log(whoWon(boardArray));
+    });
+  };
 
-    if (clicks === 9) {
-      alert("Game Over")
-    };
-    console.log(boardArray);
-    return boardArray;
-  });
-  // function winner() {
-  //     if(boardArray[0] + boardArray[1] + boardArray[2] === 3 || boardArray[3] + boardArray[4] + boardArray[5] === 3 || boardArray[6] + boardArray[7] + boardArray[8] === 3 || boardArray[0] + boardArray[3] + boardArray[6] === 3 || boardArray[1] + boardArray[4] + boardArray[7] === 3 || boardArray[2] + boardArray[5] + boardArray[8] === 3 || boardArray[0] + boardArray[4] + boardArray[8] === 3 || boardArray[2] + boardArray[4] + boardArray[6] === 3 ) {
-  //       alert("X wins");
-  //     } else if (boardArray[0] + boardArray[1] + boardArray[2] === -3 || boardArray[3] + boardArray[4] + boardArray[5] === -3 || boardArray[6] + boardArray[7] + boardArray[8] === -3 || boardArray[0] + boardArray[3] + boardArray[6] === -3 || boardArray[1] + boardArray[4] + boardArray[7] === -3 || boardArray[2] + boardArray[5] + boardArray[8] === -3 || boardArray[0] + boardArray[4] + boardArray[8] === -3 || boardArray[2] + boardArray[4] + boardArray[6] === -3 )
-  //     {
-  //       alert("O wins");
-  //     }
-  // };
-  // $("#field2").click(function(){
-  //   $(this).unbind('click');
-  //   if(clicks === 0){
-  //     $(this).css('background-image', 'url("images/x.png")');
-  //     ++clicks;
-  //   }else{
-  //     $(this).css('background-image', 'url("images/o.png")');
-  //     --clicks;
-  //   }
-  // });
-  // $("#field3").click(function(){
-  //   $(this).unbind('click');
-  //   if(clicks === 0){
-  //     $(this).css('background-image', 'url("images/x.png")');
-  //     ++clicks;
-  //   }else{
-  //     $(this).css('background-image', 'url("images/o.png")');
-  //     --clicks;
-  //   }
-  // });
-  // $("#field4").click(function(){
-  //   $(this).unbind('click');
-  //   if(clicks === 0){
-  //     $(this).css('background-image', 'url("images/x.png")');
-  //     ++clicks;
-  //   }else{
-  //     $(this).css('background-image', 'url("images/o.png")');
-  //     --clicks;
-  //   }
-  // });
-  // $("#field5").click(function(){
-  //   $(this).unbind('click');
-  //   if(clicks === 0){
-  //     $(this).css('background-image', 'url("images/x.png")');
-  //     ++clicks;
-  //   }else{
-  //     $(this).css('background-image', 'url("images/o.png")');
-  //     --clicks;
-  //   }
-  // });
-  // $("#field6").click(function(){
-  //   $(this).unbind('click');
-  //   if(clicks === 0){
-  //     $(this).css('background-image', 'url("images/x.png")');
-  //     ++clicks;
-  //   }else{
-  //     $(this).css('background-image', 'url("images/o.png")');
-  //     --clicks;
-  //   }
-  // });
-  // $("#field7").click(function(){
-  //   $(this).unbind('click');
-  //   if(clicks === 0){
-  //     $(this).css('background-image', 'url("images/x.png")');
-  //     ++clicks;
-  //   }else{
-  //     $(this).css('background-image', 'url("images/o.png")');
-  //     --clicks;
-  //   }
-  // });
-  // $("#field8").click(function(){
-  //   $(this).unbind('click');
-  //   if(clicks === 0){
-  //     $(this).css('background-image', 'url("images/x.png")');
-  //     ++clicks;
-  //   }else{
-  //     $(this).css('background-image', 'url("images/o.png")');
-  //     --clicks;
-  //   }
-  // });
-  // $("#field9").click(function(){
-  //   $(this).unbind('click');
-  //   if(clicks === 0){
-  //     $(this).css('background-image', 'url("images/x.png")');
-  //     ++clicks;
-  //   }else{
-  //     $(this).css('background-image', 'url("images/o.png")');
-  //     --clicks;
-  //   }
-  // });
+function resetGame(){
+
+}
+
 });
