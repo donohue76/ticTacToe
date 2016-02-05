dfc884a49d71e3d45b906977eb8d1ca46ea279b0
LEARN <learn@LEARN03.local>
2016-02-04 16:38:29 -0800

    end of day 2

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   $("#playerInputs").hide();
-  $("#playerDisplay").hide();
+  $("#score").hide();
+  $(".grid").hide();
   $("#start").click(function(){
     $("#startBtn").hide();
     $("#playerInputs").show();
@@ -15,22 +16,38 @@ $(document).ready(function(){
       $("#names h2").html(player1 + " vs. " + player2);
       $("#name1").html(player1);
       $("#name2").html(player2);
-      $("#playerDisplay").show();
+      $("#score").show();
+      $(".grid").fadeIn(2000);
       ticTacToeGame();
     }
   })
+  function ticTacToeGame(){
+    var boardArray = [0,0,0,0,0,0,0,0,0];
+    var clicks = 0;
+    $(".box").click(function(){
+      $(this).off('click');
+      if (clicks % 2 === 0){
+        $(this).fadeIn(1000).css('background-image', 'url("images/x.png")');
+        boardArray[parseInt(this.id)] = 1;
+      } else if (clicks % 2 === 1) {
+        $(this).css('background-image', 'url("images/o.png")');
+        boardArray[parseInt(this.id)] = -1;
+      };
+      clicks++;
+      console.log(boardArray);
+      console.log(whoWon(boardArray));
+    });
+  };
   $("#reset").click(function(){
+    var player1 = $("#player1").val();
+    var player2 = $("#player2").val();
     $(".box").css('background-image','');
+    $("#names h2").html(player1 + " vs. " + player2);
     $(".grid").bind('click', ticTacToeGame());
+
   })
-  function whoWon(arr) {
-    if(findSum(arr) === 3) {
-      return "X is the winner";
-    } else if (findSum(arr) === -3) {
-      return "O is the winner";
-    } else if (findSum(arr) === 0) {
-      return "No winner yet";
-    }
+  function sum(arr,x,y,z) {
+    return arr[x] + arr[y] + arr[z];
   };
   function findSum(arr) {
     if (sum(arr,0,1,2) === 3 || sum(arr,3,4,5) === 3 || sum(arr,6,7,8) === 3 || sum(arr,0,3,6) === 3 || sum(arr,1,4,7) === 3 || sum(arr,2,5,8) === 3 || sum(arr,2,4,6) === 3 || sum(arr,0,4,8) === 3) {
@@ -41,25 +58,23 @@ $(document).ready(function(){
       return 0;
     };
   };
-  function sum(arr,x,y,z) {
-    return arr[x] + arr[y] + arr[z];
+  var player1Arr = [];
+  var player2Arr = [];
+  function whoWon(arr) {
+    var player1 = $("#player1").val();
+    var player2 = $("#player2").val();
+    if(findSum(arr) === 3) {
+      player1Arr.push(1);
+      $("#names h2").html(player1 + " is the winner!");
+      $("#player1Score p").html(player1Arr.length);
+      $(".box").off('click');
+    } else if (findSum(arr) === -3) {
+      player2Arr.push(1);
+      $("#names h2").html(player2 + " is the winner!");
+      $("#player2Score p").html(player2Arr.length);
+;      $(".box").off('click')
+    } else if (findSum(arr) === 0) {
+      return "No winner yet";
+    }
   };
-  function ticTacToeGame(){
-    var boardArray = [0,0,0,0,0,0,0,0,0];
-    var clicks = 0;
-    $(".box").click(function(){
-      $(this).off('click');
-      if (clicks % 2 === 0){
-        $(this).css('background-image', 'url("images/x.png")');
-        boardArray[parseInt(this.id)] = 1;
-      } else if (clicks % 2 === 1) {
-        $(this).css('background-image', 'url("images/o.png")');
-        boardArray[parseInt(this.id)] = -1;
-      };
-      ++clicks;
-      console.log(boardArray);
-      console.log(whoWon(boardArray));
-    });
-  };
-
 });
